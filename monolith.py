09865aed91b314cb0b063367b41f10ec931d7933
Lucas Hagel <lucas.hagel@disroot.org>
2020-01-28 20:19:50 -0700

    Border placed, get closest intersection looks natural

--- monolith.py
+++ monolith.py
@@ -14,11 +14,11 @@ import pygame.sprite
 
 from math import sin, cos, sqrt, atan2, isclose, pi
 
-FPS = 30
-
+FPS = 20
 SIZE = (640,480)
+
 # change this when we start adding multiple layers
-title = "Test Scene"
+title = "Got Any Grapes?"
 
 #initialize pygame modules
 pygame.init()
@@ -31,9 +31,6 @@ hitsurf = font.render("Hit!!! Oops!!", 1, (255,255,255))
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption(title)
-
-# gets the image surface, RL things!
-#ScreenImage = pygame.surfarray.array3d(pygame.display.get_surface())
 
 def Quit():
     pygame.display.quit()
@@ -55,6 +52,8 @@ class Point:
         self.angle = angle
     def __str__(self):
         return "Point: " + str(self.x) + " " + str(self.y)
+    def asIntTuple(self):
+        return (round(self.x), round(self.y) )
 class Ray:
     def __init__(self, a,b):
         #a is the reference position, b is mouse position
@@ -63,6 +62,8 @@ class Ray:
     def __str__(self):
         return "Point a: (" + str(self.a.x)+ ", " + str(self.a.y) + ") " + \
                "Point b: (" + str(self.b.x)+ ", " + str(self.b.y) + ") "
+    def getPoints(self):
+        return [self.a.asIntTuple(), self.b.asIntTuple()]
 class LinePointAndAngle:
     def __init__(self, lp0, lp1):
         self.lp0 = lp0
@@ -97,19 +98,12 @@ def getIntersection(ray, segment):
     aPx, aPy = r_px+r_dx*T1,r_py+r_dy*T1
     angle = atan2( aPy - r_py, aPx - r_px)
     return Point(r_px+r_dx*T1,r_py+r_dy*T1,angle,T1)
-def display(showgrid=False, pos=[0,0]):
-    '''
-    A test function for visualization features
-    The intent is to test features that will become agent layers here 
-    '''
-    screen.fill( WHITE )
-    
-    # rp - reference position
-    rp = np.array(SIZE)//2
-    
-    #2D Visibility calculation tests
+
+def draw_static_shapes():
     w,h = SIZE
-    mapPolyLineSeg = ( (int(w*4/20), int(h*2/11)),
+    mapPolyLineSeg = ( 
+
+                     (int(w*4/20), int(h*2/11)),
                      (int(w*3/20), int(h*5/11)),
                      (int(w*5/20), int(h*7/11)),
                      (int(w*6/20), int(h*1/11)),
@@ -118,48 +112,71 @@ def display(showgrid=False, pos=[0,0]):
                      (int(w*12/20), int(h*10/11)),
                      (int(w*6/20), int(h*9/11)) 
                     )
+    border = (
+            (0, 0),
+            (w,0),
+            (w,h),
+            (0,h),
+            (0,0)
+        )
     segments = []
-    #draw all line segments for the shapes/polygons
     for i in range(len(mapPolyLineSeg)):
         line = None
         if i %4 == 3:
-            line = pygame.draw.line(screen, GREY,mapPolyLineSeg[i], mapPolyLineSeg[i-3] ,1)
+            line = pygame.draw.line(screen, GREY,mapPolyLineSeg[i], mapPolyLineSeg[i-3], 1)
             segments.append(Ray(mapPolyLineSeg[i], mapPolyLineSeg[i-3]))
         else:
-            line = pygame.draw.line(screen, GREY,mapPolyLineSeg[i], mapPolyLineSeg[i+1] ,1 )
+            line = pygame.draw.line(screen, GREY,mapPolyLineSeg[i], mapPolyLineSeg[i+1], 1)
             segments.append(Ray(mapPolyLineSeg[i], mapPolyLineSeg[i+1]))
+    for i in range(len(border)-1):
+        line = pygame.draw.line(screen, GREY, border[i], border[i+1], 1)
+        segments.append(Ray(border[i], border[i+1]) )
     pos = pygame.mouse.get_pos()
     b = pygame.draw.circle(screen, BLUE, pos, 4)
 
-    MPosToCornerRays = []
-    uniquePoints = []
-    for segment in segments:
-        for seg in [segment.a, segment.b]:
-            MPosToCornerRays.append( Ray(pos, (seg.x, seg.y) ) )
-            uniquePoints.append( (seg.x, seg.y) )
-    # project this to either closest edge or to the edge of the screen
-    uniquePoints = list(set(uniquePoints))
-    uniqueAngles = []
-    for pt in uniquePoints:
-        angle = atan2( pt[1] - pos[1], pt[0] - pos[0])
-        uniqueAngles.extend( (angle - 0.00001, angle, angle + 0.00001) )
-
-    LinePoints = []
-    for ray in MPosToCornerRays:
-        closestIntersect = None
-        for segment in segments:
-            intersect =  getIntersection(ray, segment)
-            if not intersect:    
-                continue
-            if (not closestIntersect or intersect.T1 < closestIntersect.T1):
-                closestIntersect = intersect
-        if closestIntersect:
-            LinePoints.append([ pos ,(int(closestIntersect.x), int(closestIntersect.y))])
-        else:
-            LinePoints.append([ pos , (ray.b.x, ray.b.y) ])
-    for lp in LinePoints:
-        pygame.draw.line(screen, GREEN, lp[0], lp[1], 1)     
+    segPts = [i for i in mapPolyLineSeg]
+    segPts.extend([i for i in border])
+    return segments, pos, b, border, segPts
+def display(showgrid=False, pos=[0,0]):
+    '''
+    A test function for visualization features
+    The intent is to test features that will become agent layers here 
+    '''
+    screen.fill( WHITE )
     
+    center_screen = np.array(SIZE)//2
+    # rp - reference position
+    rp = center_screen
+    
+    segments, pos, b, border, segPts = draw_static_shapes()
+    
+    incrementsOf50 = []
+    for i in segPts:
+        angle = atan2(i[1]-pos[1], i[0]-pos[0])
+        incrementsOf50.append(angle)
+        incrementsOf50.append(angle+.0001)
+        incrementsOf50.append(angle-.0001)
+    incrementsOf50.sort()
+    intersects = []
+    for angle in incrementsOf50:
+        dx = cos(angle)
+        dy = sin(angle)
+        ray = Ray(pos, [pos[0] + dx, pos[1] + dy])
+        closestIntersect = None
+        for seg in segments:
+            intersect = getIntersection(ray,seg)
+            if not intersect:
+                continue
+            if not closestIntersect or intersect.T1 < closestIntersect.T1:
+                closestIntersect = intersect
+        intersects.append(closestIntersect)
+    
+    for intersect in intersects:
+        if intersect:
+            pt = intersect.asIntTuple()
+            pygame.draw.line(screen,RED,pos, pt, 1)
+
+
     pygame.display.flip()
     return b
 
@@ -170,7 +187,6 @@ if __name__=='__main__':
     # until we need a dedicated event manager, then we'll despair
     while True:
         for event in pygame.event.get():
-            #self.eventHandle(event)
             if event.type == QUIT:
                 Quit()
             elif event.type == KEYDOWN:                
@@ -179,6 +195,7 @@ if __name__=='__main__':
             elif event.type == KEYUP:
                 velx, vely = 0,0
         keys_pressed = pygame.key.get_pressed()
+        pos = list(keys_pressed)
         spritex, spritey = pos[0], pos[1]
         if keys_pressed[K_LEFT] or keys_pressed[K_a]:
             spritex -= 5
@@ -193,6 +210,4 @@ if __name__=='__main__':
 
         # a is rectangle, b is circle for position
         b = display(pos=pos)
-        
-
         FPSCLOCK.tick(FPS)
